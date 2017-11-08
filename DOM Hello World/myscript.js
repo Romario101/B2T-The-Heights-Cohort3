feff7f712ed837274235b4c1036101584d08ad9f
Romario101 <mario_brown2212@yahoo.com>
2017-11-08 15:12:34 -0500

    my Data type task

---
+++ DOM Hello World/myscript.js
@@ -0,0 +1,14 @@
+console.log("This is an external JS File");
+document.getElementById("first").innerHTML = "brownies are good";
+console.log(5 + 2);
+console.log("I am" + 78 + "years old");
+console.log("Number: " + 25 + 12);
+console.log("Number: " + (25 + 12));
+
+var x;
+			x = true;
+			console.log(x);
+
+			var y = "54";
+			console.log(y);
+			
